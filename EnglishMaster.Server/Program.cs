267a8435354109b9717e127d4c58e36faaef9c71
fukicycle <lego.sato.4135@gmail.com>
2024-03-13 17:43:56 +0900

    Add cors settings

--- EnglishMaster.Server/Program.cs
+++ EnglishMaster.Server/Program.cs
@@ -1,5 +1,23 @@
+using EnglishMaster.Shared;
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Generate jwt key
+RandomNumberGenerator.Fill(ApplicationSettings.JWT_KEY);
+
+//Setttings Security
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://localhost:7132", "https://fukicycle.github.io")
+                .WithMethods("GET", "POST", "OPTIONS")
+                .WithHeaders("Authorization", "Content-Type")
+                .AllowCredentials();
+    });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
