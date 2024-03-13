4f93677d7b08c4dd29aa47cfc355ff208fb60617
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 08:30:59 +0900

    add page base

---
+++ EnglishMaster.Client/Pages/PageBase.cs
@@ -0,0 +1,22 @@
+﻿using EnglishMaster.Client.Stores;
+using Microsoft.AspNetCore.Components;
+
+namespace EnglishMaster.Client.Pages
+{
+    public abstract class PageBase : ComponentBase, IDisposable
+    {
+        [Inject]
+        public IStateContainer StateContainer { get; private set; } = null!;
+
+        protected override void OnInitialized()
+        {
+            StateContainer.OnMessageChanged += StateHasChanged;
+            StateContainer.OnLoadingStateChanged += StateHasChanged;
+        }
+        public void Dispose()
+        {
+            StateContainer.OnMessageChanged -= StateHasChanged;
+            StateContainer.OnLoadingStateChanged -= StateHasChanged;
+        }
+    }
+}
