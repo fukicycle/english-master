367c8390a35027ef5efe2fcfcb8a44111769e7b7
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 15:27:56 +0900

    add dictionary service

---
+++ EnglishMaster.Server/Services/DictionaryService.cs
@@ -0,0 +1,27 @@
+﻿using EnglishMaster.Server.Services.Interfaces;
+using EnglishMaster.Shared.Dto.Response;
+using EnglishMaster.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishMaster.Server.Services
+{
+    public sealed class DictionaryService : IDictionaryService
+    {
+        private readonly DB _db;
+        private readonly ILogger<DictionaryService> _logger;
+
+        public DictionaryService(DB db, ILogger<DictionaryService> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        public IList<DictionaryResponseDto> GetDictionaryResponseDtos()
+        {
+            return _db.MeaningOfWords
+                .Include(a => a.Word).Include(a => a.PartOfSpeech)
+                .Select(a => new DictionaryResponseDto(a.WordId, a.Word.Word1, a.Meaning, a.PartOfSpeech.InJapanese))
+                .ToList();
+        }
+    }
+}
