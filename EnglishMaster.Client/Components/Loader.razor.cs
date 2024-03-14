f1ebc9e4edcc9b0524fad45d1f8ffcfc39244501
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 09:30:01 +0900

    ローディングコンポーネントを追加

---
+++ EnglishMaster.Client/Components/Loader.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace EnglishMaster.Client.Components
+{
+    public partial class Loader
+    {
+        private string _style = "display: none;";
+
+        protected override void OnInitialized()
+        {
+            StateContainer.OnLoadingStateChanged += OnLoadingStateChanged;
+        }
+
+        private void DisplayLoader()
+        {
+            _style = "display: flex;";
+        }
+
+        private void HideLoader()
+        {
+            _style = "display: none;";
+        }
+
+        private void OnLoadingStateChanged()
+        {
+            if (StateContainer.IsLoading)
+            {
+                DisplayLoader();
+            }
+            else
+            {
+                HideLoader();
+            }
+        }
+    }
+}
