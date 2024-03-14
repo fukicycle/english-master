adda68b8f1db45716f243d025ff49cad9d85e63f
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 15:18:58 +0900

    辞書ページを調整

---
+++ EnglishMaster.Client/Components/WordCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EnglishMaster.Client.Components
+{
+    public partial class WordCard
+    {
+        [Parameter]
+        public long Id { get; set; }
+        public string _word = string.Empty;
+        public string _description = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            _word = "Sample word";
+            _description = "動詞,名刺";
+        }
+    }
+}
