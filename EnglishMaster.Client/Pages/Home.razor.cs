021440914c8dfd1c7ca5eaa718f9ac10c39df643
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 09:39:50 +0900

    add sample loading page

---
+++ EnglishMaster.Client/Pages/Home.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace EnglishMaster.Client.Pages
+{
+    public partial class Home : PageBase
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await RunActionWithLoading(async () =>
+            {
+                await Task.Delay(2000);
+            });
+        }
+    }
+}
---
+++ EnglishMaster.Client/Pages/Study.razor.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EnglishMaster.Client.Pages
+{
+    public partial class Study : PageBase
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await RunActionWithLoading(async () =>
+            {
+                await Task.Delay(2000);
+            });
+        }
+    }
+}
