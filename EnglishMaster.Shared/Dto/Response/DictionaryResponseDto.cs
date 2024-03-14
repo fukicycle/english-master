4c65bf73b88bbfde5c57ec832915bff9c3f9a781
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 15:21:31 +0900

    add dictionary response dto

---
+++ EnglishMaster.Shared/Dto/Response/DictionaryResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishMaster.Shared.Dto.Response
+{
+    public sealed class DictionaryResponseDto
+    {
+        public DictionaryResponseDto(long id, string word, string meaning, string partOfSpeech)
+        {
+            Id = id;
+            Word = word;
+            Meaning = meaning;
+            PartOfSpeech = partOfSpeech;
+        }
+        public long Id { get; }
+        public string Word { get; }
+        public string Meaning { get; }
+        public string PartOfSpeech { get; }
+    }
+}
