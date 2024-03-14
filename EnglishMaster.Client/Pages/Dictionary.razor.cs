085701fb20da8fd1a5a1f8126c376045f502dbe9
fukicycle <lego.sato.4135@gmail.com>
2024-03-14 13:37:07 +0900

    暫定的においていたLoadingを削除

--- EnglishMaster.Client/Pages/Dictionary.razor.cs
+++ EnglishMaster.Client/Pages/Dictionary.razor.cs
@@ -3,12 +3,5 @@ namespace EnglishMaster.Client.Pages
 {
     public partial class Dictionary : PageBase
     {
-        protected override async Task OnInitializedAsync()
-        {
-            await RunActionWithLoading(async () =>
-            {
-                await Task.Delay(1000);
-            });
-        }
     }
 }
