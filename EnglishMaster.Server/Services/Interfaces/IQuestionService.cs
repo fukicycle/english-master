2ec9ac4668727c03dff187c509e6034c1362a2d1
fukicycle <lego.sato.4135@gmail.com>
2024-03-15 11:13:22 +0900

    add services

---
+++ EnglishMaster.Server/Services/Interfaces/IQuestionService.cs
@@ -0,0 +1,7 @@
+﻿using EnglishMaster.Shared;
+
+namespace EnglishMaster.Server;
+public interface IQuestionService
+{
+    IList<QuestionResponseDto> GetQuestionResponseDtos(long partOfSpeechId, long levelId = 0, int numberOfQuestions = 50);
+}
---
+++ EnglishMaster.Server/Services/QuestionService.cs
@@ -0,0 +1,50 @@
+﻿using EnglishMaster.Shared;
+using EnglishMaster.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishMaster.Server;
+
+public sealed class QuestionService : IQuestionService
+{
+    private readonly DB _db;
+    private readonly ILogger<QuestionService> _logger;
+
+    public QuestionService(DB db, ILogger<QuestionService> logger)
+    {
+        _db = db;
+        _logger = logger;
+    }
+    public IList<QuestionResponseDto> GetQuestionResponseDtos(long partOfSpeechId, long levelId = 0, int numberOfQuestions = 50)
+    {
+        IEnumerable<MeaningOfWord> originals = _db.MeaningOfWords.Include(a => a.Word).ToList();
+        IEnumerable<MeaningOfWord> meaningOfWords = Filter(originals, partOfSpeechId, levelId);
+        IList<QuestionResponseDto> questionResponseDtos = new List<QuestionResponseDto>();
+        foreach (MeaningOfWord meaningOfWord in meaningOfWords.Take(numberOfQuestions))
+        {
+            IEnumerable<MeaningOfWord> answerTargets = Filter(originals, meaningOfWord.PartOfSpeechId, meaningOfWord.LevelId);
+            IEnumerable<MeaningOfWord> randomAnswers = GetRandomChoices(answerTargets);
+            IList<AnswerResponseDto> answerResponseDtos = new List<AnswerResponseDto>();
+            foreach (MeaningOfWord answer in randomAnswers)
+            {
+                AnswerResponseDto answerResponseDto = new AnswerResponseDto(answer.WordId, answer.Meaning);
+                answerResponseDtos.Add(answerResponseDto);
+            }
+            QuestionResponseDto questionResponseDto = new QuestionResponseDto(meaningOfWord.Id, meaningOfWord.Word.Word1, meaningOfWord.PartOfSpeechId, meaningOfWord.LevelId, answerResponseDtos);
+            questionResponseDtos.Add(questionResponseDto);
+        }
+        return questionResponseDtos;
+    }
+
+    private IEnumerable<MeaningOfWord> GetRandomChoices(IEnumerable<MeaningOfWord> answerTargets, int numberOfAnswer = 4)
+    {
+        IEnumerable<MeaningOfWord> randoms = answerTargets.OrderByDescending(a => Guid.NewGuid()).Take(numberOfAnswer);
+        return randoms;
+    }
+
+    private IEnumerable<MeaningOfWord> Filter(IEnumerable<MeaningOfWord> items, long partOfSpeechId, long levelId)
+    {
+        if (partOfSpeechId != 0) return Filter(items.Where(a => a.PartOfSpeechId == partOfSpeechId), 0, levelId);
+        if (levelId != 0) return Filter(items.Where(a => a.LevelId == levelId), levelId, 0);
+        return items;
+    }
+}
