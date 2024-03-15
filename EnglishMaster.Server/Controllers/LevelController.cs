48466c4f15cd397d3c674c8a746decdc4585e3ec
fukicycle <lego.sato.4135@gmail.com>
2024-03-15 11:51:45 +0900

    add controllers

---
+++ EnglishMaster.Server/Controllers/LevelController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishMaster.Server;
+
+[Route("/api/v1/levels")]
+public sealed class LevelController : ControllerBase
+{
+    private readonly ILevelService _levelService;
+    public LevelController(ILevelService levelService)
+    {
+        _levelService = levelService;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IActionResult GetLevels()
+    {
+        try
+        {
+            return Ok(_levelService.GetLevelResponseDtos());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
