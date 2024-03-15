a35768454ce238aaa77687df3f0899c2b360b3b4
fukicycle <lego.sato.4135@gmail.com>
2024-03-15 11:40:29 +0900

    add reponse dto

---
+++ EnglishMaster.Shared/Dto/Response/LevelResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EnglishMaster.Shared;
+
+public sealed class LevelResponseDto
+{
+    public LevelResponseDto(long id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public long Id { get; }
+    public string Name { get; }
+}
