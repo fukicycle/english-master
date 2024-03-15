7865f5f0882fd5a2e1666fb9d7410913a8cce1e7
fukicycle <lego.sato.4135@gmail.com>
2024-03-15 11:38:00 +0900

    update controller

---
+++ EnglishMaster.Server/Controllers/QuestionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishMaster.Server;
+
+[Route("/api/v1/questions")]
+public sealed class QuestionController : ControllerBase
+{
+    private readonly IQuestionService _questionService;
+    public QuestionController(IQuestionService questionService)
+    {
+        _questionService = questionService;
+    }
+
+    [HttpGet]
+    [Route("part-of-speeches/{partSpeechId}/levels/{levelId}")]
+    public IActionResult GetQuestions(long partSpeechId, long levelId)
+    {
+        try
+        {
+            return Ok(_questionService.GetQuestionResponseDtos(partSpeechId, levelId));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("part-of-speeches/{partSpeechId}/levels/{levelId}/number-of-question/{numberOfQuestion}")]
+    public IActionResult GetQuestionsWithNumberOfQuestions(long partSpeechId, long levelId, int numberOfQuestion)
+    {
+        try
+        {
+            return Ok(_questionService.GetQuestionResponseDtos(partSpeechId, levelId, numberOfQuestion));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
