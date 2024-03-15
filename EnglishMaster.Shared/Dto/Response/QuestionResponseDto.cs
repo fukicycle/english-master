0adbf2f07087a8cfd038f400901c3635a58cf332
fukicycle <lego.sato.4135@gmail.com>
2024-03-15 11:13:12 +0900

    add resposne dto

---
+++ EnglishMaster.Shared/Dto/Response/QuestionResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace EnglishMaster.Shared;
+
+public class QuestionResponseDto
+{
+    public QuestionResponseDto(long id,string word,long partOfSpeechId,long levelId,IEnumerable<AnswerResponseDto> answerResponseDtos)
+    {
+        Id = id;
+        Word = word;
+        PartOfSpeechId = partOfSpeechId;
+        LevelId = levelId;
+        AnswerResponseDtos = answerResponseDtos;
+    }
+    public long Id { get; }
+    public string Word { get;  }
+    public long PartOfSpeechId { get; }
+    public long LevelId { get; }
+    public IEnumerable<AnswerResponseDto> AnswerResponseDtos { get; }
+}
