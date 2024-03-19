5dd2c8ab83bdbb7a8242b2ac5ba9a1b0a5766e4f
fukicycle <lego.sato.4135@gmail.com>
2024-03-19 15:45:03 +0900

    add toggle switch

---
+++ EnglishMaster.Client/Components/ToggleSwitch.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EnglishMaster.Client.Components
+{
+    public partial class ToggleSwitch
+    {
+        [Parameter]
+        public bool IsSelected { get; set; }
+
+        private string _identifier = Guid.NewGuid().ToString();
+    }
+}
