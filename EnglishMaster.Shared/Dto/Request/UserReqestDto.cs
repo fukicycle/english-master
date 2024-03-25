9ccf978a31f105be37ed4e83679dadc14bea9315
fukicycle <lego.sato.4135@gmail.com>
2024-03-25 17:29:35 +0900

    fix null warning

--- EnglishMaster.Shared/Dto/Request/UserReqestDto.cs
+++ EnglishMaster.Shared/Dto/Request/UserReqestDto.cs
@@ -8,7 +8,7 @@ namespace EnglishMaster.Shared.Dto.Request
 {
     public sealed class UserReqestDto
     {
-        public UserReqestDto(string username, string password, string firstName, string lastName, string nickname)
+        public UserReqestDto(string username, string password, string firstName, string lastName, string? nickname)
         {
             Username = username;
             Password = password;
@@ -20,6 +20,6 @@ namespace EnglishMaster.Shared.Dto.Request
         public string Password { get; }
         public string FirstName { get; }
         public string LastName { get; }
-        public string Nickname { get; }
+        public string? Nickname { get; }
     }
 }
