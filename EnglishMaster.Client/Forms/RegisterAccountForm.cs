50cf08a7718a68d4420610796bec963aba9be452
fukicycle <lego.sato.4135@gmail.com>
2024-03-25 09:41:25 +0900

    update register account page style

---
+++ EnglishMaster.Client/Forms/RegisterAccountForm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnglishMaster.Client.Forms
+{
+    public sealed class RegisterAccountForm
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "This field is required.")]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "This field is required.")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Compare("Password", ErrorMessage = "Re-type password doesn't match.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
--- EnglishMaster.Client/Pages/Register.razor.cs
+++ EnglishMaster.Client/Pages/Register.razor.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+﻿using EnglishMaster.Client.Forms;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace EnglishMaster.Client.Pages;
 
 public partial class Register : PageBase
 {
+    public RegisterAccountForm Form { get; set; } = new RegisterAccountForm();
     private void CancelButtonOnClick()
     {
         NavigationManager.NavigateToLogout("authentication/logout", "");
+    }
+
+    private async Task RegisterAccount()
+    {
+        await Task.CompletedTask;
     }
 }
