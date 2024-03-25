264a04bcd512e7b5f0bc8d77ff88aa2d7c8294e8
fukicycle <lego.sato.4135@gmail.com>
2024-03-25 15:43:20 +0900

    add message dialog

---
+++ EnglishMaster.Client/Components/MessageBox.razor.cs
@@ -0,0 +1,39 @@
+﻿using EnglishMaster.Client.Pages;
+using System.Diagnostics;
+
+namespace EnglishMaster.Client.Components
+{
+    public partial class MessageBox
+    {
+        private string _display = "display: none;";
+
+        protected override void OnInitialized()
+        {
+            StateContainer.OnMessageChanged += StateContainer_OnMessageChanged;
+        }
+
+        private void StateContainer_OnMessageChanged()
+        {
+            if (StateContainer.Message == string.Empty)
+            {
+                _display = "display: none;";
+            }
+            else
+            {
+                _display = "display: block;";
+            }
+            StateHasChanged();
+        }
+
+        private void CloseButtonOnClick()
+        {
+            StateContainer.Message = string.Empty;
+        }
+
+        private void OkButtonOnClick()
+        {
+            StateContainer.Message = string.Empty;
+            NavigationManager.NavigateTo(NavigationManager.Uri, true);
+        }
+    }
+}
