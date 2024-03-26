da249e181d021b726baa44154807b035f639334d
fukicycle <lego.sato.4135@gmail.com>
2024-03-26 19:22:41 +0900

    change method to NavigateToLogin

--- EnglishMaster.Client/Pages/Home.razor.cs
+++ EnglishMaster.Client/Pages/Home.razor.cs
@@ -1,10 +1,12 @@
-﻿namespace EnglishMaster.Client.Pages
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace EnglishMaster.Client.Pages
 {
     public partial class Home : PageBase
     {
         private void LoginButtonOnClick()
         {
-            NavigationManager.NavigateTo($"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
+            NavigationManager.NavigateToLogin($"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
         }
     }
 }
