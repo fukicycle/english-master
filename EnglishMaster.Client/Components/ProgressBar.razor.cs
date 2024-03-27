8ef75cd8e20116eb36316801c460b4dcded6cad4
fukicycle <lego.sato.4135@gmail.com>
2024-03-27 17:01:19 +0900

    add progress circle and progress bar

---
+++ EnglishMaster.Client/Components/ProgressBar.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EnglishMaster.Client.Components
+{
+    public partial class ProgressBar
+    {
+        [Parameter]
+        public double Value { get; set; }
+        [Parameter]
+        public bool HasDisplayValue { get; set; }
+        private string _style = "width:0%;";
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            if (Value < 0)
+            {
+                throw new NotSupportedException($"Not support value:{Value}");
+            }
+            if (Value > 100)
+            {
+                throw new NotSupportedException($"Not support value:{Value}");
+            }
+            _style = $"width:{Value}%;";
+        }
+
+        private string GetDisplayValue()
+        {
+            return $"{Math.Round(Value, 1)}%";
+        }
+    }
+}
