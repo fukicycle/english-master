d679e663b5b1f3ced5883b8b3a8d6ac5871aba2b
fukicycle <lego.sato.4135@gmail.com>
2024-03-29 10:03:12 +0900

    add dtos

---
+++ EnglishMaster.Shared/Dto/Request/ResultRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EnglishMaster.Shared.Dto.Request
+{
+    public sealed class ResultRequestDto
+    {
+        public ResultRequestDto(string email, long questionMeaningOfWordId, long answerMeaningOfWordId)
+        {
+            Email = email;
+            QuestionMeaningOfWordId = questionMeaningOfWordId;
+            AnswerMeaningOfWordId = answerMeaningOfWordId;
+        }
+        public string Email { get; }
+        public long QuestionMeaningOfWordId { get; }
+        public long AnswerMeaningOfWordId { get; }
+    }
+}
