3de9bb5dbbddb5825772f4947a97bcfe125b94b6
fukicycle <lego.sato.4135@gmail.com>
2024-03-29 14:32:20 +0900

    最低収録単語数を設定化した

--- EnglishMaster.Shared/ApplicationSettings.cs
+++ EnglishMaster.Shared/ApplicationSettings.cs
@@ -4,6 +4,7 @@
     {
         public static ApplicationMode Mode = ApplicationMode.Dev;
         public static byte[] JWT_KEY = new byte[64];
+        public static int NUMBER_OF_MIN_LIMIT = 20;
 
         //DO NOT CHANGE THIS ID
         public const string APPLICATION_ID = "7bc622cfe27a4a9d90314625a82b673b";
