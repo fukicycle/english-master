7068adc0216cf8e4f9ef7a5f3c312b1e836d5c9c
fukicycle <lego.sato.4135@gmail.com>
2024-03-29 09:51:06 +0900

    add result page

---
+++ EnglishMaster.Client/Pages/Result.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EnglishMaster.Client.Pages
+{
+    public partial class Result : PageBase
+    {
+        [Parameter]
+        [SupplyParameterFromQuery]
+        public int Count { get; set; }
+
+        private void CloseButtonOnClick()
+        {
+            NavigationManager.NavigateTo("");
+        }
+    }
+}
