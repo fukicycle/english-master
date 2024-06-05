812633d210ae8cb575b92d71d4484af63aefc35d
fukicycle <lego.sato.4135@gmail.com>
2024-06-05 09:39:26 +0900

    remove chart library

--- EnglishMaster.Client/Pages/Achievement.razor.cs
+++ EnglishMaster.Client/Pages/Achievement.razor.cs
@@ -25,4 +25,23 @@ public partial class Achievement
     {
         NavigationManager.NavigateToLogin($"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
     }
+
+
+
+
+
+    private List<MyData> Data { get; set; } = new();
+    protected override void OnInitialized()
+    {
+        Data.Add(new MyData { Category = "Jan", NetProfit = 12, Revenue = 33 });
+        Data.Add(new MyData { Category = "Feb", NetProfit = 43, Revenue = 42 });
+        Data.Add(new MyData { Category = "Mar", NetProfit = 112, Revenue = 23 });
+    }
+
+    public class MyData
+    {
+        public string Category { get; set; }
+        public int NetProfit { get; set; }
+        public int Revenue { get; set; }
+    }
 }
