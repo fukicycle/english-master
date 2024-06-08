0870d9df34173334746ea21bd83049bc130a8b90
fukicycle <lego.sato.4135@gmail.com>
2024-06-08 09:36:08 +0900

    Add http context helper

---
+++ EnglishMaster.Server/Controllers/HttpContextHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace EnglishMaster.Server.Controllers
+{
+    public static class HttpContextHelper
+    {
+        public static long GetUserId(this HttpContext context)
+        {
+            IEnumerable<Claim> claims = context.User.Claims;
+            if (!claims.Any(a => a.Type == ClaimTypes.NameIdentifier))
+            {
+                throw new Exception("Invalid http context.");
+            }
+            string userIdValue = claims.First(a => a.Type == ClaimTypes.NameIdentifier).Value;
+            return Convert.ToInt64(userIdValue);
+        }
+    }
+}
