5be247ca60181b7a086f2254c4ff9534407cd9e5
fukicycle <lego.sato.4135@gmail.com>
2024-07-04 10:24:21 +0900

    Add flush mode page.

--- EnglishMaster.Client/Pages/Study/FlushMode.razor.cs
+++ EnglishMaster.Client/Pages/Study/FlushMode.razor.cs
@@ -5,22 +5,10 @@ namespace EnglishMaster.Client.Pages.Study;
 
 public partial class FlushMode
 {
-    [Parameter]
-    public long LevelId { get; set; } = 0;
-
-    [Parameter]
-    public long PartOfSpeechId { get; set; } = 0;
 
     [Parameter]
     public bool AutoStart { get; set; } = false;
 
-    private long _partOfSpeechId = 0;
-    private long _levelId = 0;
     private QuestionResponseDto? _question = new QuestionResponseDto(1, 1, "Word", 1, 1, Enumerable.Empty<AnswerResponseDto>());
-    private List<PartOfSpeechResponseDto> _partOfSpeeches = new List<PartOfSpeechResponseDto>();
-    private List<LevelResponseDto> _levles = new List<LevelResponseDto>();
-    private bool _isAnswered = false;
-    private bool _isCorrect = false;
-    private int _questionNumber = 0;
-    private bool _isMute = false;
+    private bool _isShowAnswered = false;
 }
---
+++ EnglishMaster.Shared/Dto/Response/FlushCardResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace EnglishMaster.Shared.Dto.Response;
+
+public sealed class FlushCardResponseDto
+{
+    public string Word { get; }
+    public long WordId { get; set; }
+}
