048dcfb971d59b82f245b86bd71413809859b289
Fuki Sato <lego.sato.4135@gmail.com>
2024-07-04 21:25:53 +0900

    Fixedgit add .
    quit
    [

--- EnglishMaster.Client/Program.cs
+++ EnglishMaster.Client/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddHttpClient(nameof(ApplicationMode.Prod), httpClient =>
 {
     httpClient.BaseAddress = new Uri("https://www.sato-home.mydns.jp:9445");
 });
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(ApplicationSettings.Mode)));
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(ApplicationSettings.Mode.ToString()));
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSpeechSynthesis();
 builder.Services.AddScoped<ISettingService, SettingService>();
