16dc29b44b9a378120796054f00291c6cd3b8ba7
Fuki Sato <lego.sato.4135@gmail.com>
2024-07-04 22:43:39 +0900

    fix environment bugs.

--- EnglishMaster.Client/Services/SettingService.cs
+++ EnglishMaster.Client/Services/SettingService.cs
@@ -8,17 +8,24 @@ namespace EnglishMaster.Client.Services
     public sealed class SettingService : ISettingService
     {
         private readonly ILocalStorageService _localStorageService;
+        private readonly SpeakService _speakService;
         private const string USER_SETTINGS_STORAGE_KEY = $"{ApplicationSettings.APPLICATION_ID}_SETTING";
-        private readonly UserSettings _defaultSettings = new UserSettings("Samantha|en-US", 10, StudyMode.Choice);
+        private readonly UserSettings _defaultSettings = new UserSettings("", 10, StudyMode.Choice);
 
-        public SettingService(ILocalStorageService localStorageService)
+        public SettingService(ILocalStorageService localStorageService, SpeakService speakService)
         {
             _localStorageService = localStorageService;
+            _speakService = speakService;
         }
 
         public async Task<UserSettings> LoadAsync()
         {
             UserSettings? currentSettings = await _localStorageService.GetItemAsync<UserSettings>(USER_SETTINGS_STORAGE_KEY);
+            if (currentSettings == null)
+            {
+                var voices = await _speakService.GetVoicesAsync();
+                _defaultSettings.VoiceIdentity = voices.First().VoiceIdentity;
+            }
             return currentSettings ?? _defaultSettings;
         }
 
